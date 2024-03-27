26f2d71d6872ae73a753710dc3e06a1ea535b687
Punit-Choudhary <itspunitchoudhary@gmail.com>
2024-03-27 11:20:00 +0530

    Gradient generator

---
+++ gradientgenerator/script.js
@@ -0,0 +1,38 @@
+let firstColor = document.getElementById('first-color');
+let secondColor = document.getElementById('second-color');
+const outputCode = document.getElementById('code');
+// let copyBtn = document.getElementById('copy');
+const tooltip = document.querySelector('.tooltip');
+const heading = document.getElementById('heading');
+
+let currentDirection = 'to bottom';
+
+
+function setDirection(value, _this) {
+    let directions = document.querySelectorAll(".buttons button");
+    for (let i of directions) {
+        i.classList.remove("active");
+    }
+
+    _this.classList.add("active");
+    currentDirection = value
+}
+
+function generateCode() {
+    outputCode.value = `background-image: linear-gradient(${currentDirection}, ${firstColor.value}, ${secondColor.value})`;
+    document.getElementsByTagName("body")[0].style.backgroundImage = `linear-gradient(${currentDirection}, ${firstColor.value}, ${secondColor.value})`;
+    heading.style.background = `linear-gradient(${currentDirection}, ${firstColor.value}, ${secondColor.value})`;
+    heading.style.backgroundClip = 'text';
+}
+
+function copyCode() {
+    navigator.clipboard.writeText(outputCode.value);
+    // copyBtn.textContent = "Copied";
+    tooltip.classList.add("show");
+    setTimeout(() => {
+        tooltip.classList.remove("show");
+        // copyBtn.textContent = "Copy";
+    }, 1000);
+}
+
+generateCode();
